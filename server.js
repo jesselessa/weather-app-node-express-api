2829eb48dc7e623bb121e16efa18c04d5712064d
jesselessa <jessica.elessa@gmail.com>
2023-12-02 11:16:49 +0100

    completed function to get weather data from any city

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import cors from "cors";
+import fetch from "node-fetch";
 
 const app = express();
 const port = process.env.PORT;
+const host = process.env.HOST;
 
 //-------------- MIDDLEWARES ---------------//
+app.use(cors());
 
 //---------------- ROUTES ------------------//
 
+//* Get data from any city
+app.get("/data/:city", (req, res) => {
+  const city = req.params.city;
+  const apiKey = process.env.API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      return res.json(data);
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.json(error);
+    });
+});
+
 //------------- START SERVER ---------------//
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}.`);
+  console.log(`Server listening at http://${host}:${port}`);
 });
