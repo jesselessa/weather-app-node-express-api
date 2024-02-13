f93d3878e879f19044fd0b51439bc6836f120194
jesselessa <jessica.elessa@gmail.com>
2024-02-13 20:32:19 +0100

    updated server port in server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import fetch from "node-fetch";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT
 const host = process.env.HOST;
 
 //-------------- MIDDLEWARE ---------------//
