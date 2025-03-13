14753f50b3b557c73d1609a56a656551ba22757b
jesselessa <jessica.elessa@gmail.com>
2025-03-14 00:26:11 +0100

    updated server port number

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import fetch from "node-fetch";
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3001;
 
 //-------------- MIDDLEWARES ---------------//
 app.use(express.json());
